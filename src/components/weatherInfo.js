ee01a7d1ac16ea34d61072b366441243d55e72e3
Giorgio Zanni <g.zanni@hotmail.com>
2019-02-05 16:48:34 -0300

    ADD: weather info component

--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const WeatherInfo = () => {
-  return(
-    <div className="info_box">
-      <p>City</p>
-      <p>Current time</p>
-      <p>Current temp</p>
-    </div>
-  )
+class WeatherInfo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      weather: '',
+      description: '',
+      temp: 0.0,
+      city: '',
+      time: ''
+    }
+  }
+  
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      let lat = position.coords.latitude,
+          lon = position.coords.longitude;
+      console.log(lat, lon)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=85b4be71576890900bfcd3a946908d9e`)
+      .then( response => response.json())
+      .then( data => this.setState({
+        weather: data.weather[0].main,
+        description: data.weather[0].description,
+        temp: data.main.temp,
+        city: data.name
+      }))
+    })
+    this.getTime();
+  }
+
+  getTime = () => {
+    const currTime = new Date();
+    this.setState({
+      time: `${currTime.getHours()}:${currTime.getMinutes()}`
+    })
+  }
+
+  render(){
+    return(
+      <div className="info_box">
+        <p>City: {this.state.city}</p>
+        <p>Current time {this.state.time}</p>
+        <p>Current temp: {this.state.temp}°C</p>
+      </div>
+    )
+  }
 }
 
 export default WeatherInfo;
