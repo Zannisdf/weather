fc7ab67647810967cfded2ffeb41a9e6a0ab53c4
Giorgio Zanni <g.zanni@hotmail.com>
2019-02-05 18:02:36 -0300

    refactor

--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -4,8 +4,10 @@ class WeatherInfo extends Component {
   constructor(props){
     super(props);
     this.state = {
+      error: false,
       weather: '',
       description: '',
+      icon: '01n',
       temp: 0.0,
       city: '',
       time: ''
@@ -14,31 +16,40 @@ class WeatherInfo extends Component {
   
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
-      let lat = position.coords.latitude,
-          lon = position.coords.longitude;
-      console.log(lat, lon)
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=85b4be71576890900bfcd3a946908d9e`)
-      .then( response => response.json())
-      .then( data => this.setState({
-        weather: data.weather[0].main,
-        description: data.weather[0].description,
-        temp: data.main.temp,
-        city: data.name
-      }))
-    })
+      const currPos = position.coords;
+      this.getCurrWeather(currPos.latitude, currPos.longitude);
+    }, this.error)
     setInterval(this.getTime, 1000)
   }
+
+  getCurrWeather = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=85b4be71576890900bfcd3a946908d9e`)
+    .then( response => response.json())
+    .then( data => this.setState({
+      weather: data.weather[0].main,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+      temp: data.main.temp,
+      city: data.name
+    }));
+  }
+
+  error = () => this.setState({ error: true });
+
+  formatTime = n => n < 10 ? `0${n}` : n; 
 
   getTime = () => {
     const currTime = new Date();
     this.setState({
-      time: `${currTime.getHours()}:${currTime.getMinutes()}:${currTime.getSeconds()}`
+      time: `${this.formatTime(currTime.getHours())}:${this.formatTime(currTime.getMinutes())}`
     })
   }
 
   render(){
     return(
       <div className="info_box">
+        <p>{this.state.error ? "There's been an error, please try again" : ''}</p>
+        <img src={`https://openweathermap.org/img/w/${this.state.icon}.png`} alt="Weather icon"></img>
         <p>Weather: {this.state.weather}</p>
         <p>{this.state.description}</p>
         <p>City: {this.state.city}</p>
