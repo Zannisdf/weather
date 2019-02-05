cbc3efb2b7020920a557e685a8eacc1b692afaca
Giorgio Zanni <g.zanni@hotmail.com>
2019-02-05 20:52:13 -0300

    App reestructure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
-import Tab from './components/tab'
 import WeatherInfo from './components/weatherInfo'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      weather: '',
+      description: '',
+      icon: '',
+      temp: 0.0,
+      city: '',
+      time: ''
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      const currPos = position.coords;
+      this.getCurrWeather(currPos.latitude, currPos.longitude);
+    }, this.error)
+    setInterval(this.getTime, 1000)
+  }
+
+  getCurrWeather = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=85b4be71576890900bfcd3a946908d9e`)
+    .then( response => response.json())
+    .then( data => this.setState({
+      weather: data.weather[0].main,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+      temp: data.main.temp,
+      city: data.name
+    }));
+  }
+
+  error = () => this.setState({ error: true });
+
+  formatTime = n => n < 10 ? `0${n}` : n; 
+
+  getTime = () => {
+    const currTime = new Date();
+    this.setState({
+      time: `${this.formatTime(currTime.getHours())}:${this.formatTime(currTime.getMinutes())}`
+    })
+  }
+
   render() {
+    const { icon, weather, description, city, time, temp } = this.state;
     return (
       <div className="App">
-        <Tab />
-        <WeatherInfo />
+        <WeatherInfo 
+          icon={icon}
+          weather={weather}
+          description={description}
+          city={city}
+          time={time}
+          temp={temp}
+          />
       </div>
     );
   }
--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -1,63 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class WeatherInfo extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      error: false,
-      weather: '',
-      description: '',
-      icon: '01n',
-      temp: 0.0,
-      city: '',
-      time: ''
-    }
-  }
-  
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      const currPos = position.coords;
-      this.getCurrWeather(currPos.latitude, currPos.longitude);
-    }, this.error)
-    setInterval(this.getTime, 1000)
-  }
-
-  getCurrWeather = (lat, lon) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=85b4be71576890900bfcd3a946908d9e`)
-    .then( response => response.json())
-    .then( data => this.setState({
-      weather: data.weather[0].main,
-      description: data.weather[0].description,
-      icon: data.weather[0].icon,
-      temp: data.main.temp,
-      city: data.name
-    }));
-  }
-
-  error = () => this.setState({ error: true });
-
-  formatTime = n => n < 10 ? `0${n}` : n; 
-
-  getTime = () => {
-    const currTime = new Date();
-    this.setState({
-      time: `${this.formatTime(currTime.getHours())}:${this.formatTime(currTime.getMinutes())}`
-    })
-  }
-
-  render(){
-    return(
-      <div className="info_box">
-        <p>{this.state.error ? "There's been an error, please try again" : ''}</p>
-        <img src={`https://openweathermap.org/img/w/${this.state.icon}.png`} alt="Weather icon"></img>
-        <p>Weather: {this.state.weather}</p>
-        <p>{this.state.description}</p>
-        <p>City: {this.state.city}</p>
-        <p>Current time {this.state.time}</p>
-        <p>Current temp: {this.state.temp}°C</p>
-      </div>
-    )
-  }
+const WeatherInfo = ({ icon, weather, description, city, time, temp }) => {
+  return(
+    <div className="info_box">
+      <img src={`https://openweathermap.org/img/w/${icon}.png`} alt="Weather icon"></img>
+      <p>Weather: {weather}</p>
+      <p>{description}</p>
+      <p>City: {city}</p>
+      <p>Current time {time}</p>
+      <p>Current temp: {temp}°C</p>
+    </div>
+  )
 }
 
 export default WeatherInfo;
